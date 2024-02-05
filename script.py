e76028a0a8f7b3dcef14889cd74b7a9281024f0e
tromma <67795550+tromma@users.noreply.github.com>
2024-02-05 14:05:09 +0000

    feat: clean code

--- script.py
+++ script.py
@@ -1,19 +1,26 @@
 import unittest
 
-def names(prenoms):
+def count_names_exceeding_length(prenoms: list[str]) -> int:
+    """
+    Count the number of names exceeding a specific length.
+
+    :param prenoms: List of names.
+    :return: Count of names longer than the specified length.
+    """
+    NUMBER_OF_LETTERS = 7
     more_than_seven = 0
     for prenom in prenoms:
-        if len(prenom) > 7:
+        if len(prenom) > NUMBER_OF_LETTERS:
             more_than_seven += 1
-            print("Prenom supérieur à 7 : " + prenom)
+            print(f"Prenom supérieur à {NUMBER_OF_LETTERS} : {prenom}")
         else:
-            print("Prenom inférieur ou égal à 7 : " + prenom)
+            print(f"Prenom inférieur ou égal à {NUMBER_OF_LETTERS} : {prenom}")
     return more_than_seven
 
 class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
+    def test_names(self) -> None:
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
+        more_than_seven = count_names_exceeding_length(prenoms=prenoms)
         self.assertEqual(more_than_seven, 4)
 
 if __name__ == '__main__':
